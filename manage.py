56e5d93083e026184c79202ab307c490f5b5ecba
Admin <da8y_01@yahoo.com>
2018-02-26 14:14:12 -0500

    Adds manage.py .

---
+++ manage.py
@@ -0,0 +1,18 @@
+import os
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+from xkcd import app
+from xkcd.app import create_app
+from xkcd.database import db
+
+
+migrate = Migrate(app, db)
+#manager = Manager(app)
+manager = Manager(create_app)
+
+manager.add_command('db', MigrateCommand)
+
+
+if __name__ == '__main__':
+    manager.run()
